3984c34bb30daab7a155c33a9bed0e7d7bbbb48e
Michael Montero <monterom334@gmail.com>
2026-01-21 21:30:44 -0400

    refactor(import): remove initSystemKnex method and utilize TenantModel for tenant queries

--- packages/server/src/modules/Import/jobs/ImportDeleteExpiredFilesJob.ts
+++ packages/server/src/modules/Import/jobs/ImportDeleteExpiredFilesJob.ts
@@ -1,16 +1,13 @@
 import { Cron } from '@nestjs/schedule';
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { ClsService } from 'nestjs-cls';
 import { ImportDeleteExpiredFiles } from '../ImportRemoveExpiredFiles';
 import { TenantModel } from '../../System/models/TenantModel';
-import Knex from 'knex';
 
 @Injectable()
 export class ImportDeleteExpiredFilesJobs {
   constructor(
     private readonly importDeleteExpiredFiles: ImportDeleteExpiredFiles,
-    private readonly configService: ConfigService,
     private readonly cls: ClsService,
     @Inject(TenantModel.name)
     private readonly tenantModel: typeof TenantModel,
@@ -24,9 +21,10 @@ export class ImportDeleteExpiredFilesJobs {
     try {
       console.log('Delete expired import files has started.');
       
-      // Get all initialized tenants
-      const systemKnex = this.initSystemKnex();
-      const tenants = await systemKnex('tenants').whereNotNull('initializedAt');
+      // Get all initialized tenants using TenantModel
+      const tenants = await (this.tenantModel as any)
+        .query()
+        .whereNotNull('initializedAt');
       
       // Process each tenant
       for (const tenant of tenants) {
@@ -44,18 +42,5 @@ export class ImportDeleteExpiredFilesJobs {
     } catch (error) {
       console.error('Error in importDeleteExpiredJob:', error);
     }
-  }
-
-  private initSystemKnex(): any {
-    return Knex({
-      client: 'mysql2',
-      connection: {
-        host: this.configService.get('systemDatabase.host'),
-        user: this.configService.get('systemDatabase.user'),
-        password: this.configService.get('systemDatabase.password'),
-        database: this.configService.get('systemDatabase.databaseName'),
-        charset: 'utf8',
-      },
-    });
   }
 }
